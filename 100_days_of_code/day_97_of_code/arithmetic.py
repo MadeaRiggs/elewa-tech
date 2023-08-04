60b1093fc299b19aa7b8732f79a863becf9930f5
MadeaRiggs <mukamiclaus@gmail.com>
2023-08-04 22:21:20 +0300

    "arranging scripts"

---
+++ 100_days_of_code/day_97_of_code/arithmetic.py
@@ -0,0 +1,27 @@
+a= 70
+b= 60
+c= 40
+
+#addition
+d= a + b+ c
+print("sum is" , d)
+
+#subtraction
+e= a -b
+print("subtraction is ", e)
+
+#multiplication
+f= a * c
+print("multiplication is ", f)
+
+#division
+g= b / c
+print("division is ", g)
+
+#modulus
+h= b % c
+print("modulus is ", h)
+
+#exponential
+i= c ** 2
+print("exponential is ", i)
