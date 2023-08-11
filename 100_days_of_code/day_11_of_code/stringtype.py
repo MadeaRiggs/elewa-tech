411d3f07f17e811c4136b6e8fa5d8ab9ce8e2917
MadeaRiggs <mukamiclaus@gmail.com>
2023-08-11 21:42:42 +0300

    string operations

---
+++ 100_days_of_code/day_11_of_code/stringtype.py
@@ -0,0 +1,33 @@
+var= "My name is Jane"
+
+#length of variable
+print("length of variable is: ", len(var))
+
+#lower case
+print("lower case of variable is: ", var.lower())
+
+#upper case
+print("upper case of variable is: ", var.upper())
+
+#splitting
+print("splitted variable is: ", var.split())
+
+#strip
+print("stripped variable is: ", var.strip(""))
+
+#replacing
+print("replaced variable is: ", var.replace("Jane", "Alice"))
+
+#concatenation
+str_1= " She is good"
+print("concatenated variable is: ", var + str_1)
+
+#datatype identification
+a= 54
+b= True
+c= 34.90
+
+print("The data type of variable is ", type(str_1))
+print("The data type of a is ", type(a))
+print("The data type of b is ", type(b))
+print("The data type of c is ", type(c))
