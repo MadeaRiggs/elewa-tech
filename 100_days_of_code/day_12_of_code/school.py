a14e8905b11ddd0fe49a9c03cbfe02be6d04d07b
MadeaRiggs <mukamiclaus@gmail.com>
2023-08-14 22:38:37 +0300

    collecting school details

---
+++ 100_days_of_code/day_12_of_code/school.py
@@ -0,0 +1,31 @@
+#################SCHOOL DETAILS
+#Person Identifier
+# print("Kindly fill in the following details:")
+name= print("What is your name? ", input())
+stream= print("Which class are you in? ", input())
+teacher= print("Who is your teacher? ", input())
+
+name = input("What is your name? ")
+stream = input("Which class are you in? ")
+teacher = input("Who is your teacher? ")#
+
+# #Performance
+print("What was your performance in these subjects?")
+math= int(print(input()))
+eng= int(print(input()))
+phy= int(print(input()))
+
+math = int(input("Enter your math score: "))
+eng = int(input("Enter your English score: "))
+phy = int(input("Enter your physics score: "))
+
+# #Co-curricular
+print("What co curricular activities did you participate in?")
+co_curr= input()
+
+#final print statement
+print("My name is ", name)
+print("I am in class ", stream)
+print("I have a score of ", eng, "in English")
+print("I have participated in ", co_curr, "activity")
+
