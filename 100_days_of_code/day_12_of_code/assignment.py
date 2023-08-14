da49c7ac759fd516ee42c429b7e14a3cbe67691b
MadeaRiggs <mukamiclaus@gmail.com>
2023-08-14 22:37:22 +0300

    assignment

---
+++ 100_days_of_code/day_12_of_code/assignment.py
@@ -0,0 +1,30 @@
+# 1) Write a python Program to prompt the user to create a shopping list and calculate the total cost
+# milk= int(input("Milk packets are "))
+# flour= int(input("Flour packets are "))
+# oil= int(input("Cooking oil bottles are "))
+
+# milk_cost= float(input("The cost of milk is "))
+# flour_cost= float(input("The cost of flour is "))
+# oil_cost= float(input("The cost of oil is "))
+
+# total_cost= (milk * milk_cost) + (flour * flour_cost) + (oil * oil_cost)
+# print("The total shopping cost is ", total_cost)
+
+
+# 2) Write a python program that will prompt the user to perform the calculations of Area, surface area and volume of a cuboid
+# length = float(input("length is "))
+# width = float(input("width is "))
+# height = float(input("height is "))
+
+# # Calculate and display the results
+# area = length * width
+# print("Area of cuboid is ", area)
+
+# surface_area = (2 *length * width) + (2 *length * height) + (2 * height * width)
+# print("Surface area of cuboid is ", surface_area)
+
+# volume = length * width * height
+# print("Volume of cuboid is ", volume)
+
+
+# 3) Write a python program that will prompt the user to input their credit card information and check whether the information shared is a string or an integer
