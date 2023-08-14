1c812d52a229bc56283c0b2e70f967849cc8916b
MadeaRiggs <mukamiclaus@gmail.com>
2023-08-14 22:37:52 +0300

    computations on input functions

---
+++ 100_days_of_code/day_12_of_code/computations.py
@@ -0,0 +1,14 @@
+######################COMPUTATIONS
+#Area, Perimeter and Volume
+length= int(input("Length is "))
+width= int(input("width is "))
+height= int(input("height is "))
+
+area= length * width
+print("Area is ", area)
+
+perimeter= 2 * (length + width)
+print("Perimeter is ", perimeter)
+
+volume= length * width * height
+print("Volume is ", volume)
