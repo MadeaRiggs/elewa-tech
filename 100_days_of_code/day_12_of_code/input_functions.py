c5409e3f5a48ae90c603cfccd91007ef0612ce37
MadeaRiggs <mukamiclaus@gmail.com>
2023-08-14 22:38:11 +0300

    introduction to input functions

---
+++ 100_days_of_code/day_12_of_code/input_functions.py
@@ -0,0 +1,29 @@
+# # using input functions
+#raw_input() is only used in Python 2 NOT Python 3
+
+# print("How old are you?, ")
+# age= raw_input()
+
+# print("How tall are you?, ")
+# height= raw_input()
+
+# print("How much do you weigh?, ")
+# weight= raw_input()
+
+# print("So you are %r old, %r tall and %r weight", age, height, weight)
+
+#using input functions
+
+print("How old are you?")
+age = input()
+
+print("How tall are you?")
+height = input()
+
+print("How much do you weigh?")
+weight = input()
+
+# print("So you are %r old, %r tall and %r weight" % (age, height, weight))
+print("So you are", age , "old", height, "tall and", weight, "weight" )
+
+
