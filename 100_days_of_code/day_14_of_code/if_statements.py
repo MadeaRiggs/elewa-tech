a293b37d91b5194c48cf3d46e22b4bbe20ffd2f7
MadeaRiggs <mukamiclaus@gmail.com>
2023-08-17 00:37:46 +0300

    if statements

---
+++ 100_days_of_code/day_14_of_code/if_statements.py
@@ -0,0 +1,17 @@
+########### IF STATEMENTS
+age= 17
+
+if (age >= 18):
+    print("You are an adult")
+
+print("You are underage")
+
+############ IF ELSE STATEMENT
+hse_a= 10000
+hse_b= 15000
+budget= 12000
+
+if (budget<= 12000):
+    print(hse_a, "House is affordable")
+else: 
+    print(hse_b, "House is expensive")
