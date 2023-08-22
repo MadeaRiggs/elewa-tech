548cf5536b636c9c9b398decb679da836d957b15
MadeaRiggs <mukamiclaus@gmail.com>
2023-08-23 00:50:40 +0300

    nested if statements

---
+++ 100_days_of_code/day_14_to_19_of_code/nestedif.py
@@ -0,0 +1,22 @@
+# Deciding what to wear
+weather = "rainy"
+status = 1
+color = "grey"
+occasion = "official"
+comfort= 1
+
+if (weather == "sunny") :
+    if (color == "blue") :
+        print("Wear the cloth")
+    elif (status ==1 ) :
+        print("Wear the cloth")
+    else :
+        print("Wear anything clean")
+
+else :
+    if (color == "grey") :
+        print("Check the weather condition")
+    elif (comfort == 1) :
+        print("You can wear it")
+    else :
+        print("Look for something else to wear")
