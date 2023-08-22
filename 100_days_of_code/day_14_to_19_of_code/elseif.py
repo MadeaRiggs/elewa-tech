0eac1014d056db4c2302c2c43d912788f317827c
MadeaRiggs <mukamiclaus@gmail.com>
2023-08-23 00:50:06 +0300

    else if statements

---
+++ 100_days_of_code/day_14_to_19_of_code/elseif.py
@@ -0,0 +1,14 @@
+# No Power Examples
+power = 0
+training = 2
+assn = 0
+
+#Elif structure
+if(power == 1):
+    print("Conduct class")
+elif(training == 0):
+    print(" Post-pone class")
+elif(training == 1):
+    print("Give out Assignment")
+else:
+    print("Assign Take home Assignment")
