e55e221b80107d296ff52d41bc89f6fee60747d9
MadeaRiggs <mukamiclaus@gmail.com>
2023-09-07 21:34:01 +0300

    types of errors in python

---
+++ 100_days_of_code/day_28_to_29_of_code/error_types.py
@@ -0,0 +1,32 @@
+##########Types of Errors
+
+#Zero Division Error
+a= 0
+b= 1
+print(b / a)
+
+#Type Error- operations between variables of different data types
+name= "Delilah"
+age= 34
+activity= name + age
+print(activity)
+
+#Name Error 1- Undefined variable
+bar= "suswe"
+bar_2= "happyland"
+# print(bar_3)
+
+#Name Error 2- case sensitive variable
+bar= "suswe"
+bar_2= "happyland"
+# print(Bar_2)
+
+#Syntax Error
+bags= 3
+soap= 4
+
+# if (bags > 8) ;
+#     print(bags)
+
+#Indentation error- wrong code indentation
+
