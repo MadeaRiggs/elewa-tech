5b8de8ec7431a53db869e68eb62d4398b0bb8c1c
MadeaRiggs <mukamiclaus@gmail.com>
2023-09-11 21:48:00 +0300

    read operations

--- 100_days_of_code/day_31_of_code/read_mode.py
+++ 100_days_of_code/day_31_of_code/read_mode.py
@@ -10,22 +10,22 @@ a-append and write respectively
 #Case study 1
 
 #Reading a file
-# var_name = open("100_days_of_code/day_31_of_code/sip.txt",  "r")
-# # print(var_name)
+var_name = open("100_days_of_code/day_31_of_code/sip.txt",  "r")
+# print(var_name)
 
-# for each in var_name:
-#     print(var_name)
+for each in var_name:
+    print(var_name)
 
 
 #second demo
-# file_read= open("100_days_of_code/day_31_of_code/sip.txt", "r")
-# print(file_read.read())
+file_read= open("100_days_of_code/day_31_of_code/sip.txt", "r")
+print(file_read.read())
 
 
 #third demo
-# with open("100_days_of_code/day_31_of_code/sip.txt") as file_read:
-#     text = file_read.read()
-#     print(text)
+with open("100_days_of_code/day_31_of_code/sip.txt") as file_read:
+    text = file_read.read()
+    print(text)
 
 #performing split operation on file
 with open("100_days_of_code/day_31_of_code/sip.txt") as data:
