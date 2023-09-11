6a464decc6ecf8047740b4a84b5e6f7cdc5c74c9
MadeaRiggs <mukamiclaus@gmail.com>
2023-09-11 21:42:01 +0300

    Append operations

---
+++ 100_days_of_code/day_31_of_code/append_mode.py
@@ -0,0 +1,9 @@
+##################FILE HANDLING
+#Append operations
+
+#Append mode case study 1
+var_name = open("100_days_of_code/day_31_of_code/sip.txt",  "a")
+var_name.write("I can't find my pen")
+var_name.write("I can't find anything")
+
+var_name.close()
