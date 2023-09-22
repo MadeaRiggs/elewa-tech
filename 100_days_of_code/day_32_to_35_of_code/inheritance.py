4dad279ba6e0a23a5b37ba164047f197555f7498
MadeaRiggs <mukamiclaus@gmail.com>
2023-09-22 11:58:02 +0300

    inheritance in python classes

--- 100_days_of_code/day_32_to_35_of_code/inheritance.py
+++ 100_days_of_code/day_32_to_35_of_code/inheritance.py
@@ -1,115 +1,115 @@
 ##########INHERITANCE IN PYTHON
 
 #########CASE STUDY 1
-#creating base class
-# class Person:
-#     #creating a constructor
-#     #the parameters are Attributes but they are initialized to become rules
-#     def __init__(self, name, age):
-#         self.name= name
-#         self.age= age
+# creating base class
+class Person:
+    #creating a constructor
+    #the parameters are Attributes but they are initialized to become rules
+    def __init__(self, name, age):
+        self.name= name
+        self.age= age
 
-#     #method to get the name
-#     def getName(self):
-#         return self.name
+    #method to get the name
+    def getName(self):
+        return self.name
     
-#     #method to get the age
-#     def getAge(self):
-#         return self.age 
+    #method to get the age
+    def getAge(self):
+        return self.age 
 
-# #derived class: Education 
-# class Education(Person):
+#derived class: Education 
+class Education(Person):
 
-#     def isEducated(self):
-#         return True
+    def isEducated(self):
+        return True
 
-# #create an object
-# person_1= Person("Mukami Kami", 20)
-# person_1= Education("Mukami Kami", 20)   
+#create an object
+person_1= Person("Mukami Kami", 20)
+person_1= Education("Mukami Kami", 20)   
 
-# #print statements
-# print(person_1.isEducated())
-# print(person_1.getAge())
+#print statements
+print(person_1.isEducated())
+print(person_1.getAge())
     
 
 ###########CASE STUDY 2
-# class Electric_shop():
+class Electric_shop():
 
-#     #constructor
-#     def __init__(self, pd_name, quantity, price) :
-#         self.name= pd_name
-#         self.quantity= quantity
-#         self.price= price
+    #constructor
+    def __init__(self, pd_name, quantity, price) :
+        self.name= pd_name
+        self.quantity= quantity
+        self.price= price
 
-# class Methods(Electric_shop):
+class Methods(Electric_shop):
 
-#     #method 1
-#     def getName(self):
-#         return self.name
+    #method 1
+    def getName(self):
+        return self.name
     
-#     def getQuantity(self):
-#         Quantity= int(input("Please input quantity: ", ))
-#         self.quantity= Quantity
-#         return self.quantity
+    def getQuantity(self):
+        Quantity= int(input("Please input quantity: ", ))
+        self.quantity= Quantity
+        return self.quantity
     
-#     #method 3
-#     def getPrice(self):
-#         cost= int(input("Enter the cost: ", ))
-#         self.price= self.quantity * cost 
-#         return self.price
+    #method 3
+    def getPrice(self):
+        cost= int(input("Enter the cost: ", ))
+        self.price= self.quantity * cost 
+        return self.price
     
-# #object methods
-# Laptop= Methods("Hewlett Parker", 0, 0)
-# print(Laptop.getQuantity())
-# print(Laptop.getPrice())  
+#object methods
+Laptop= Methods("Hewlett Parker", 0, 0)
+print(Laptop.getQuantity())
+print(Laptop.getPrice())  
 
 
 ########CASE STUDY 3- USING super() class
-# class Electric_shop():
+class Electric_shop():
 
-#     #constructor
-#     def __init__(self, pd_name, quantity, price) :
-#         self.name= pd_name
-#         self.quantity= quantity
-#         self.price= price
+    #constructor
+    def __init__(self, pd_name, quantity, price) :
+        self.name= pd_name
+        self.quantity= quantity
+        self.price= price
 
-# #derived class 1
-# class name(Electric_shop):
+#derived class 1
+class name(Electric_shop):
 
-#     #constructor (s stands for super) Super class is when a child/derived class can be inherited from
-#     def __init__(self, pd_name, quantity, price) :
-#         self.sname= pd_name
-#         self.squantity= quantity
-#         self.qprice= price
+    #constructor (s stands for super) Super class is when a child/derived class can be inherited from
+    def __init__(self, pd_name, quantity, price) :
+        self.sname= pd_name
+        self.squantity= quantity
+        self.qprice= price
 
-#     #method 1
-#     def getName(self):
-#         name= input("Enter the device name: ", )
-#         self.name= name
-#         return self.name
+    #method 1
+    def getName(self):
+        name= input("Enter the device name: ", )
+        self.name= name
+        return self.name
     
-# #derived class 2
-# class laptop(name):
+#derived class 2
+class laptop(name):
 
-#     #method 1
-#     def getPrice(self):
-#         price= int(input("Enter the price: ", ))
-#         self.sprice= price 
-#         return self.sprice
+    #method 1
+    def getPrice(self):
+        price= int(input("Enter the price: ", ))
+        self.sprice= price 
+        return self.sprice
     
 
-# #create object
-# # lappy= laptop("Asus", 0, 0)
+#create object
+# lappy= laptop("Asus", 0, 0)
 
-# #####OR 
-# #the quotes are just used to show that there is memory allocation for the name attribute
-# lappy= laptop(" ", 0, 0)
+#####OR 
+#the quotes are just used to show that there is memory allocation for the name attribute
+lappy= laptop(" ", 0, 0)
 
-# print(lappy.getPrice())
-# print(lappy.getName())
+print(lappy.getPrice())
+print(lappy.getName())
 
 
-#########CASE STUDY 3
+#########CASE STUDY 4
 ####Creating a class that holds properties only
 #inherit the properties to create methods 
 class Fruit:
@@ -138,11 +138,13 @@ class info(Fruit):
         self.price= total
         return self.price
     
-# #create an object
-# banana= info("Banana", " ", 0)
+#create an object
+banana= info("Banana", " ", 0)
 
-# print(banana.totalPrice())
+print(banana.totalPrice())
 
+
+###########OR 
 #hardcoding the methods
 fruit_1= Fruit(" ", " ", 0)
 fruit_1_name= input("Enter the name: ", )
